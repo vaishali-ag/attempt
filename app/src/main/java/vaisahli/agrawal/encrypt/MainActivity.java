cfabd8bd13a1d5d5f4f5b251c728c3eb3feea007
Vaishali-ag <vaishali02071997@gmail.com>
2019-01-21 10:13:36 +0530

    object create and findview byid

--- app/src/main/java/vaisahli/agrawal/encrypt/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/encrypt/MainActivity.java
@@ -2,12 +2,23 @@ package vaisahli.agrawal.encrypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText2);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
     }
 }
