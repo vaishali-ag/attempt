fe1417b5c1a3cc1aab3bcf2eb7ae0301a0e7da8f
Vaishali-ag <vaishali02071997@gmail.com>
2019-01-23 20:44:24 +0530

    code for checking the given input is valid or not if not valid then try again till 3 attempts

--- app/src/main/java/vaisahli/agrawal/encrypt/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/encrypt/MainActivity.java
@@ -2,6 +2,7 @@ package vaisahli.agrawal.encrypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText2);
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer i ;
+                for (i=1;i<=3;i++)
+                {
+                    String pass = editText.getText().toString();
+                    if(pass == "vaishali")
+                    {
+                        textView.setText("welcome!!!\n ");
+                      break;
+
+                    }
+                    else
+                    {
+                        textView.setText("Warning!!! \n attempt left "+i);
+                    }
+
+                }
+            }
+        });
 
     }
 }
